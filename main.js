9a026cbefbb3236a88f83133444cc79f70e434e1
Philipp A. <flying-sheep@web.de>
2026-01-25 13:32:21 +0100

    init

---
+++ main.js
@@ -0,0 +1,98 @@
+import html from 'escape-html-template-tag'
+import { loadPyodide } from 'pyodide'
+
+const pyodide = await loadPyodide()
+await pyodide.loadPackage('micropip')
+const micropip = pyodide.pyimport('micropip')
+await micropip.install('sphinx')
+
+const ta_conf = document.querySelector('textarea#conf')
+const ta_rst = document.querySelector('textarea#rst')
+const frame_out = document.querySelector('iframe')
+
+function setup() {
+    ta_conf.addEventListener('input', run)
+    ta_rst.addEventListener('input', run)
+    run()
+}
+
+/**
+ * @template {unknown[]} Args
+ * @param {(...args: Args) => Promise<void>} f
+ * @returns {(...args: Args) => Promise<void>}
+ */
+function throttle(f) {
+    let running = false
+    /** @type {null | Args} */
+    let queued = null
+    
+    return async (...args) => {
+        if (running) { // replace queued args
+            queued = args
+            return
+        }
+        // run it
+        running = true
+        await f(args)
+        // while args have been queued in the meantime, run it again
+        while (queued !== null) {
+            const q_args = queued
+            queued = null
+            await f(...q_args)
+        }
+        running = false
+    }
+}
+
+const run = throttle(async () => {
+    pyodide.FS.writeFile('conf.py', ta_conf.value)
+    pyodide.FS.writeFile('index.rst', ta_rst.value)
+    await pyodide.loadPackagesFromImports(ta_conf.value)
+
+    try {
+        const code = await pyodide.runPythonAsync(`
+from sphinx.cmd.build import main
+main(['-b', 'html', '.', '_build'])
+`)
+        if (code !== 0) { throw new Error(code) }
+    } catch (e) {
+        frame_out.srcdoc = html`<pre><label style="color:red">${e.toString()}</label></pre>`
+        return
+    }
+    frame_out.srcdoc = fixLinks(pyodide.FS.readFile('_build/index.html', { encoding: 'utf8' }))
+})
+
+/** @param {string} html */
+function fixLinks(html) {
+    const doc = Document.parseHTMLUnsafe(html)
+    for (const link of doc.querySelectorAll('link')) {
+        if (link.rel !== 'stylesheet') { continue }
+        if (/^https?:/.test(link.href)) { continue }
+        try {
+            const style = doc.createElement('style')
+            style.append(readBuildTextFile(link.href))
+            link.replaceWith(style)
+        } catch (e) {
+            console.error(e, link)
+        }
+    }
+    for (const script of doc.querySelectorAll('script')) {
+        if (!script.src || /^https?:/.test(script.src)) { continue }
+        try {
+            script.text = readBuildTextFile(script.src)
+            script.removeAttribute("src")
+        } catch (e) {
+            console.error(e, script)
+        }
+    }
+    return doc.documentElement.outerHTML
+}
+
+/** @param {string} path */
+function readBuildTextFile(path) {
+    const url = new URL(path, "file://")
+    // console.log(`_build${url.pathname}`, path)
+    return pyodide.FS.readFile(`_build${url.pathname}`, { encoding: 'utf8' })
+}
+
+setup()
