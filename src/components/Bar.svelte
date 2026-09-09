55bbd9c50132aa83107c431598e0fa03cf4a0666
Phil Schaf <github@phil.red>
2026-09-09 11:45:39 +0200

    external sphinx icon

--- src/components/Bar.svelte
+++ src/components/Bar.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-import { SiGithub } from '@icons-pack/svelte-simple-icons'
+import { SiGithub, SiSphinx } from '@icons-pack/svelte-simple-icons'
 import { AppBar } from '@skeletonlabs/skeleton-svelte'
 import type { Snippet } from 'svelte'
 import { github } from '~build/info'
-import HorusEye from '../horus-eye.svg?component'
 
 export interface Props {
 	children?: Snippet
@@ -17,7 +16,7 @@ const { children, onshare, onreset }: Props = $props()
 <AppBar>
 	<AppBar.Toolbar class="grid-cols-[auto_1fr_auto]">
 		<AppBar.Lead>
-			<a title="Sphinx GitHub repo" href="https://github.com/sphinx-doc/sphinx" class="btn-icon btn-icon-lg preset-tonal"><HorusEye /></a>
+			<a title="Sphinx GitHub repo" href="https://github.com/sphinx-doc/sphinx" class="btn-icon btn-icon-lg preset-tonal"><SiSphinx /></a>
 		</AppBar.Lead>
 		<AppBar.Headline class="flex flex-row gap-4">
 			<p class="text-2xl">Sphinx&nbsp;Playground</p>
